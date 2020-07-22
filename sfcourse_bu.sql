f567a5d32e54cc93140dea9621d5471591976a34
Nicholas Rolando <nicklamort@gmail.com>
2020-07-21 21:21:13 -0600

    update db dump file

--- sfcourse_bu.sql
+++ sfcourse_bu.sql
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Clothes'),(2,'Dev'),(3,'Chill'),(4,'Furniture');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `post`
 --
 
@@ -25,8 +49,12 @@ DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `category_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_5A8A6C8D12469DE2` (`category_id`),
+  CONSTRAINT `FK_5A8A6C8D12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +63,7 @@ CREATE TABLE `post` (
 
 LOCK TABLES `post` WRITE;
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
-INSERT INTO `post` VALUES (3,'This is a great post'),(5,'This post sucks'),(7,'This post was the bomb'),(8,'This is a sweet post'),(9,'That post was homophobic'),(10,'sweet new post from forms');
+INSERT INTO `post` VALUES (7,'This post was the bomb','',3),(8,'This is a sweet post','',4),(14,'give me the right file ext','15a24ac7b1edf4130388c7ff3bc411dd.png',1),(15,'sweet new post from forms','d295d0b333ebb69b524d320b02efce0e.jpeg',4),(16,'This is a great post','0c1d5e293360550fa85f08a9b2d95db5.png',4),(17,'tmnt 23','147541afc0c3cc8a3dcbae720b3bdf8e.png',4),(18,'Imageless posts are not much fun',NULL,3),(19,'yo yo yo, here\'s an image','8ab479010c143bfbcf00303329798d57.jpeg',1),(20,'My first dev job','db21953cf70a59d5e43edb8862ef90c2.jpeg',2),(21,'Let\'s try adding another','f9632a45dc468c8897eb30078a0ac3cc.jpeg',4);
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-08 11:09:17
+-- Dump completed on 2020-07-21 21:20:06
